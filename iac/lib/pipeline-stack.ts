6b2ec7c773ded43d56ca8a31078b984635714f2c
dan.sweeney <dan.sweeney@citilogik.com>
2023-04-28 09:26:58 +0100

    added correct remote repo to pipeline

--- iac/lib/pipeline-stack.ts
+++ iac/lib/pipeline-stack.ts
@@ -9,7 +9,7 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'code-pipeline', {
       pipelineName: 'WebDeploymentPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('DanSweeney23/energy-monitoring', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
