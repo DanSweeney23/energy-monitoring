7904083ed66a3928b63e3847b7a05b8f2a8ca43a
dan.sweeney <dan.sweeney@citilogik.com>
2023-04-28 13:22:20 +0100

    created initial vue project

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,14 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3, aws_s3_deployment as s3Deployment } from 'aws-cdk-lib';
+import { BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { CfnDistribution, CloudFrontWebDistribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
 
 export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'deployment-bucket', {
-      versioned: true
+    const deploymentBucket = new s3.Bucket(this, 'deployment-bucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: new BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false
+      })
+    });
+
+    const deployment = new s3Deployment.BucketDeployment(this, 'deployStaticWebsite', {
+      sources: [s3Deployment.Source.asset("../frontend/dist")],
+      destinationBucket: deploymentBucket
+    });
+
+    // Define CloudFront Distribution with error responses.
+    const accessDeniedErrorResponse: CfnDistribution.CustomErrorResponseProperty = {
+      errorCode: 403,
+      errorCachingMinTtl: 30,
+      responseCode: 200,
+      responsePagePath: '/index.html',
+    };
+    const notFoundErrorResponse: CfnDistribution.CustomErrorResponseProperty = {
+      errorCode: 404,
+      errorCachingMinTtl: 30,
+      responseCode: 200,
+      responsePagePath: '/index.html',
+    };
+
+    const oai = new OriginAccessIdentity(this, 'bucket-oai', {});
+    deploymentBucket.grantRead(oai);
+    
+    const distribution = new CloudFrontWebDistribution(this, 'cf-web-distribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: deploymentBucket,
+            originAccessIdentity: oai
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+      errorConfigurations: [
+        accessDeniedErrorResponse,
+        notFoundErrorResponse
+      ]
     });
   }
 }
