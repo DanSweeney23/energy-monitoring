db43227e26a6936a941d3004a6d5227fa6e827ef
dan.sweeney <dan.sweeney@citilogik.com>
2023-04-28 10:43:45 +0100

    removed pipeline iac

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -2,16 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { IacStack } from '../lib/iac-stack';
-import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-
-new PipelineStack(app, 'PipelineStack', {
-  env: {
-    account: '278421086553',
-    region: 'eu-west-2',
-  }
-});
 
 new IacStack(app, 'IacStack', {
   env: {
