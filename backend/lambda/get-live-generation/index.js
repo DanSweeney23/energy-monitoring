c4c82953baf6332cdbfa579f0e6f35dada256831
dan.sweeney <dan.sweeney@citilogik.com>
2023-04-30 12:06:29 +0100

    renamed lambdas

---
+++ backend/lambda/get-live-generation/index.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+
+const makeDateString = (timestamp) => `${timestamp.getUTCDate()}${(timestamp.getUTCMonth()+1).toString().padStart(2, '0')}${timestamp.getUTCFullYear().toString().padStart(2, '0')}`;
+const makeTimeString = (timestamp) => `${timestamp.getUTCHours().toString().padStart(2, '0')}${timestamp.getUTCMinutes().toString().padStart(2, '0')}${timestamp.getUTCSeconds().toString().padStart(2, '0')}`;
+
+exports.handler = async function (event) {
+  const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+  
+  const maxTime = new Date();
+  maxTime.setMinutes(maxTime.getMinutes()-10);
+  
+  const params = {
+    TableName: process.env.GENERATION_TABLE_NAME,
+    ScanIndexForward: false,
+    Limit: 1,
+    KeyConditionExpression: `#date = :date AND #time > :maxtime`,
+    ExpressionAttributeValues: {  
+      ":date":{  
+        N: makeDateString(maxTime),  
+      },  
+      ":maxtime":{  
+        N: makeTimeString(maxTime),  
+      }  
+    },  
+    ExpressionAttributeNames: {
+    "#date": "date",
+    "#time": "time"
+    }
+  }
+
+  const res = await ddb.query(params).promise();
+
+  const responseBody = res.Items[0]
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(responseBody)
+  };
+  console.log("response: " + JSON.stringify(response))
+  return response;
+}
---
+++ backend/lambda/put-live-generation/index.js
@@ -0,0 +1,50 @@
+const AWS = require('aws-sdk')
+const xml2js = require('xml2js');
+const axios = require('axios');
+
+const makeDateString = (timestamp) => `${timestamp.getUTCDate()}${(timestamp.getUTCMonth()+1).toString().padStart(2, '0')}${timestamp.getUTCFullYear().toString().padStart(2, '0')}`;
+const makeTimeString = (timestamp) => `${timestamp.getUTCHours().toString().padStart(2, '0')}${timestamp.getUTCMinutes().toString().padStart(2, '0')}${timestamp.getUTCSeconds().toString().padStart(2, '0')}`;
+
+exports.handler = async function (event) {
+  const parser = xml2js.Parser();
+
+  const ssmClient = new AWS.SSM({
+    apiVersion: '2014-11-06',
+  });
+
+  const apiKeyParam = await ssmClient.getParameter({
+    Name: '/elexon/apikey',
+    WithDecryption: true,
+  }).promise();
+  const apiKey = apiKeyParam.Parameter.Value;
+
+  const elexonResponse = await axios.get(`https://downloads.elexonportal.co.uk/fuel/download/latest?key=${apiKey}`);
+
+  const parsedResponse = await parser.parseStringPromise(elexonResponse.data);
+
+
+  const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+
+  const time = parsedResponse['GENERATION_BY_FUEL_TYPE_TABLE']['INST'][0]['$']['AT'].replace(' ', 'T');
+  const dt = new Date(time)
+
+
+  const params = {
+    TableName: process.env.GENERATION_TABLE_NAME,
+    Item: {
+      'date': { 'N': makeDateString(dt) },
+      'time': { 'N': makeTimeString(dt) },
+      'fuels': {
+        'L': parsedResponse['GENERATION_BY_FUEL_TYPE_TABLE']['INST'][0]['FUEL'].map(function (row) {
+          const fuelType = row['$']['TYPE'];
+          const value = row['$']['VAL'];
+          const percent = row['$']['PCT'];
+
+          return { 'M': { value: { 'N': value }, percent: { 'N': percent }, fuelType: { 'S': fuelType } } };
+        })
+      }
+    }
+  };
+
+  await ddb.putItem(params).promise();
+}
